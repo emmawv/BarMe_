8f2e9e4ecd33d15bde5ac1e77372841a23715dd9
Emma Willett <emmawv@gmail.com>
2021-01-09 19:52:32 +0100

    github changes

--- project2/routes/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = app => {
-
-    // Base URLS
-    app.use('/', require('./base.routes.js'))
-    app.use('/', require('./auth.routes.js'))
-    app.use('/bars', require('./bar.routes.js'))
-    app.use('/api', require('./api.routes.js'))
-    app.use('/send-email', require('./email.routes.js'))
-}
